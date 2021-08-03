f69bde8edd19d31a66dc382a1a1ab1bc0a57849c
dipzcoded <dipoakerele@gmail.com>
2021-08-03 04:15:29 +0100

    tweaked

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ sr.reveal(".skills__data", { interval: 200 });
 sr.reveal(".skills__img", { delay: 400 });
 
 /*SCROLL WORK*/
-sr.reveal(".work__img", {});
+// sr.reveal(".work__img", {});
 
 /*SCROLL CONTACT*/
 sr.reveal(".contact__input", { interval: 200 });
